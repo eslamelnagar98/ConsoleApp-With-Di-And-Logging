224d7db50401a05bc1f48fa55cd0c6ef3a4dd1e1
Islam <eslamelnagar44@gmail.com>
2021-10-27 02:46:10 +0200

    Add project files.

---
+++ ConsoleUI/TestDi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class TestDi
+    {
+        private readonly IGreetingService _greetingService;
+        public TestDi(IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+
+        public void Islam()
+        {
+            _greetingService.Run();
+        }
+
+    }
+}
